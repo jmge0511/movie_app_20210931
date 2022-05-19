1e3a9b64c9a442eeebfd424678cfb42c68a72a08
jmge0511 <jmge0511@naver.com>
2022-05-19 09:48:30 +0900

    07장까지 완성된 소스코드

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
-function Food({name, picture}) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+import axios from 'axios';
+import React from 'react';
+import Movie from './Movie';
+import './App.css';
 
-const foodILike =[
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.IDrNtl4pG_zepTDko3IUhgHaHa%26pid%3DApi&f=1',
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.eMl_BEvZz0cUY_10ZnkJZwHaE9%26pid%3DApi&f=1',
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.od4YprzuR0ZOmv851wA0OAHaEx%26pid%3DApi&f=1',
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.mW17Z8uq6unL6xvle_X0YgHaHa%26pid%3DApi&f=1',
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.XiO-APPrmYLstQXFFJDGrgHaE-%26pid%3DApi&f=1',
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish =>
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
-      )}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({movies, isLoading: false});
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const {isLoading, movies} =this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+            <div className="loader">
+              <span className="loader__text">Loading</span>
+            </div>
+          ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
